8af59d3206442f057ea0d974ca381a9c9fe30535
rupalee-kumari <151054155+rupalee-kumari@users.noreply.github.com>
2023-11-28 12:28:32 +0000

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,108 @@
+import java.util.*;
+public class Array {
+    //    public static void main(String[]arg) {
+//        Scanner sc=new Scanner(System.in);
+//        int size=sc.nextInt();
+//        int numbers[]=new int[size];
+//        for(int i=0;i<size;i++) {
+//             numbers[i] =sc.nextInt();
+//        }
+//        int x=sc.nextInt();
+//        for(int i=0;i<numbers.length;i++){
+//            if(numbers[i]==x){
+//                System.out.println("x found at index:"+i);
+    public static void main(String[] arg) {
+//       Scanner sc=new Scanner(System.in);
+//      int size=sc.nextInt();
+//      String names[]=new String[size];
+//    for(int i=0;i<size;i++){
+//        names[i]=sc.next();
+//    }
+//    for(int i=0;i<names.length;i++){
+//        System.out.println("name"+(i+1)+"is:"+names[i]);
+//    }
+
+//    Scanner sc=new Scanner(System.in);
+//    int size=sc.nextInt();
+//    int number[]=new int[size];
+////    input
+//    for(int i=0;i<size;i++){
+//        number[i]=sc.nextInt();
+//   }
+//    int max=Integer.MIN_VALUE;
+//    int min=Integer.MAX_VALUE;
+////    output
+//    for(int i=0;i<number.length;i++){
+//    if(number[i]<min){
+//        min=number[i];
+//    }
+//    if(number[i]>max){
+//        max=number[i];
+//    }
+//
+//    }
+//    System.out.println("largest number is:"+max);
+//    System.out.println("smallest number is:"+min);
+//    Scanner sc=new Scanner(System.in);
+//    int size=sc.nextInt();
+//    int number[]=new int[size];
+//    for(int i=0;i<size;i++){
+//        number[i]=sc.nextInt();
+//   }
+//    boolean isAscending=true;
+//    for(int i=0;i<number.length-1;i++){
+//        if(number[i]<number[i+1]){
+//            isAscending=true;
+//        }
+//    }
+//    if(isAscending){
+//        System.out.println("array is sorted assending order");
+//    }
+//    else{
+//        System.out.println("array is not sorted in ascending order");
+//    }
+
+
+//        int size =sc.nextInt();
+//        int array[]=new int[size ];
+////        marks[0]=1;
+////        marks[1]=2;
+////        marks[2]=3;
+//       for(int i=0;i<size;i++) {
+//           System.out.println(i);
+//       }
+//
+//        Scanner sc=new Scanner(System.in);
+//        int x=sc.nextInt();
+//        int number[]=new int[a];
+
+//    }
+//    }
+//find peak number
+//        Scanner sc = new Scanner(System.in);
+//        int size= sc.nextInt();
+//        int array[]=new int[size];
+//        for( int i=0;i<size;i++){
+//            array[i]=sc.nextInt();
+//        }
+//        int x=sc.nextInt();
+//        for( int i=0;i<size;i++){
+//            if(array[i]==x){
+//                System.out.println("number found");
+//            }
+//    }
+//}}
+//        public class Leet{
+//        public  static void main(String args[])
+//        int [] num={2,7,11,15};
+//        for(int i=0,i<num.length-1,i++)
+//
+//            {
+//                for (int j = i + 1; j < num.length,j++){
+//                target = 17;
+//                if (num[i] + num[j] = target) {
+//                    System.out.print(num[j], num[j + 1])
+//                }
+//            }
+//            }
+
